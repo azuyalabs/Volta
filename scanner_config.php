d601ee41723f4bd55dac872ca2f12b89a074b30a
Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>
2019-05-12 11:14:26 +0900

    Initial commit.
    Signed-off-by: Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>

---
+++ scanner_config.php
@@ -0,0 +1,21 @@
+<?php
+
+$projectPath = __DIR__;
+
+// Declare directories which contain PHP code
+$scanDirectories = [
+    $projectPath . '/app/',
+    $projectPath . '/resources/views/',
+    $projectPath . '/routes/',
+    $projectPath . '/bootstrap/',
+];
+
+// Optionally declare standalone files
+$scanFiles = [];
+
+return [
+    'composerJsonPath' => $projectPath . '/composer.json',
+    'vendorPath' => $projectPath . '/vendor/',
+    'scanDirectories' => $scanDirectories,
+    'scanFiles' => $scanFiles
+];
