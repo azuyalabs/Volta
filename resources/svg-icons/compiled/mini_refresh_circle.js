69fec13293c27be7813c05c12a0489eb89215362
Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>
2019-05-13 23:49:41 +0900

    Recompiled SVG Icons.
    Signed-off-by: Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>

--- resources/svg-icons/compiled/mini_refresh_circle.js
+++ resources/svg-icons/compiled/mini_refresh_circle.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
-var icon = require('vue-svgicon');
+var icon = require('vue-svgicon')
 icon.register({
-    mini_refresh_circle: {
-        width: 24,
-        height: 24,
-        viewBox: '0 0 24 24',
-        data: '<circle pid="0" cx="12" cy="12" r="10"/>',
-    },
-});
+  'mini_refresh_circle': {
+    width: 24,
+    height: 24,
+    viewBox: '0 0 24 24',
+    data: '<circle pid="0" cx="12" cy="12" r="10"/>'
+  }
+})
--- resources/svg-icons/compiled/printer-extruder.js
+++ resources/svg-icons/compiled/printer-extruder.js
@@ -1,11 +1,10 @@
 /* eslint-disable */
-var icon = require('vue-svgicon');
+var icon = require('vue-svgicon')
 icon.register({
-    'printer-extruder': {
-        width: 16,
-        height: 16,
-        viewBox: '0 0 30 30',
-        data:
-            '<g _fill="none" _stroke="#000" stroke-width="1.7" stroke-linecap="round"><path pid="0" d="M6.711 16.573h6.06M17.318 16.573h5.971"/></g><g _fill="none" _stroke="#000"><path pid="1" stroke-width="1.3" stroke-linejoin="round" d="M12.971 14.643h4.102v5.977h-4.102z"/><path pid="2" d="M14.223 22.796h1.598" stroke-width="1.7" stroke-linecap="round"/></g><path pid="3" d="M8.41 6.861s.161 2.05.887 2.618c1.263.988 3.56-.624 4.793.4.815.676.843 3.062.843 3.062" _fill="none" _stroke="#000" stroke-linecap="round"/>',
-    },
-});
+  'printer-extruder': {
+    width: 16,
+    height: 16,
+    viewBox: '0 0 30 30',
+    data: '<g _fill="none" _stroke="#000" stroke-width="1.7" stroke-linecap="round"><path pid="0" d="M6.711 16.573h6.06M17.318 16.573h5.971"/></g><g _fill="none" _stroke="#000"><path pid="1" stroke-width="1.3" stroke-linejoin="round" d="M12.971 14.643h4.102v5.977h-4.102z"/><path pid="2" d="M14.223 22.796h1.598" stroke-width="1.7" stroke-linecap="round"/></g><path pid="3" d="M8.41 6.861s.161 2.05.887 2.618c1.263.988 3.56-.624 4.793.4.815.676.843 3.062.843 3.062" _fill="none" _stroke="#000" stroke-linecap="round"/>'
+  }
+})
