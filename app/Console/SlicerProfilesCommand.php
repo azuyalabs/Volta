2c46d8d9ff6f496c960158f0241d0425ce5a81b9
Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>
2019-05-14 12:37:11 +0900

    Included missing Slicer command.
    Signed-off-by: Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>

---
+++ app/Console/SlicerProfilesCommand.php
@@ -0,0 +1,292 @@
+<?php
+/**
+ * This file is part of the Volta Project.
+ *
+ * Copyright (c) 2018 - 2019. AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <me@sachatelgenhof.com>
+ */
+
+namespace App\Console;
+
+use Ramsey\Uuid\Uuid;
+use League\Plates\Engine;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+/**
+ * Class that handles generating Slicer profiles
+ *
+ * @package App\Console
+ */
+class SlicerProfilesCommand extends Command
+{
+    /**
+     * Directory name where filaments are stored
+     */
+    const FILAMENTS_DIR = 'filaments';
+
+    /**
+     * Directory name where template files are stored
+     */
+    const TEMPLATES_DIR = 'filaments/_templates';
+
+    /**
+     * Directory name where profiles will be stored
+     */
+    const PROFILES_DIR = 'filaments/profiles';
+
+    /**
+     * List of supported slicers
+     *
+     * @var array
+     */
+    protected $slicers = ['slic3rpe' => 'Slic3rPE', 'cura' => 'Ultimaker Cura', 'slic3r' => 'Slic3r'];
+
+    /**
+     * Common settings for 'Bridge Speed Fan' (by filament type)
+     *
+     * @var array
+     */
+    protected $bridge_fan_speeds = ['PLA' => 100, 'Woodfill' => 100, 'ABS' => 30, 'PET' => 50];
+
+    /**
+     * Common settings for 'Disable Fan First Layers' (by filament type)
+     *
+     * @var array
+     */
+    protected $disable_fan_first_layers = ['PLA' => 1, 'Woodfill' => 1, 'ABS' => 0, 'PET' => 3];
+
+    /**
+     * Common settings for 'Min Fan Speed' (by filament type)
+     *
+     * @var array
+     */
+    protected $min_fan_speed = ['PLA' => 100, 'Woodfill' => 100, 'ABS' => 15, 'PET' => 30];
+
+    /**
+     * Common settings for 'Max Fan Speed' (by filament type)
+     *
+     * @var array
+     */
+    protected $max_fan_speed = ['PLA' => 100, 'Woodfill' => 100, 'ABS' => 30, 'PET' => 50];
+
+    /**
+     * Mapping for the parent filament
+     *
+     * @var array
+     */
+    protected $inherits = ['PLA' => 'PLA', 'Woodfill' => 'PLA', 'ABS' => 'ABS', 'PET' => 'PET'];
+
+    /**
+     * Common settings for 'Fan Below Layer Time' (by filament type)
+     *
+     * @var array
+     */
+    protected $fan_below_layer_time = ['PLA' => 100, 'Woodfill' => 100, 'ABS' => 20, 'PET' => 20];
+
+    /**
+     * Common settings for 'Filament Max Volumetric Speed' (by filament type)
+     *
+     * @var array
+     */
+    protected $filament_max_volumetric_speed = ['PLA' => 15, 'Woodfill' => 15, 'ABS' => 11, 'PET' => 8];
+
+    /**
+     * @var string The console command name
+     */
+    protected $signature = 'volta:profiles';
+
+    /**
+     * @var string The console command description
+     */
+    protected $description = 'Generate slicer profiles';
+
+    /**
+     * Handle to Template Engine
+     *
+     * @var Engine
+     */
+    protected $plates;
+
+    /**
+     * SlicerProfilesCommand constructor.
+     */
+    public function __construct()
+    {
+        $this->plates = new Engine(storage_path('app/' . self::TEMPLATES_DIR), 'ini');
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command
+     *
+     * @throws \Exception
+     */
+    public function handle(): void
+    {
+        $filamentFiles = File::glob(storage_path('app/' . self::FILAMENTS_DIR) . '/*.json');
+
+        $cura_material_settings = [];
+
+        foreach ($filamentFiles as $filamentDefinition) {
+            $f = \json_decode(\file_get_contents($filamentDefinition), true);
+
+            $color = isset($f['color']['name']) ? $f['color']['name'] : $this->color2Name($f['color']['code']);
+            $filamentName = \join(' ', [$f['brand'], $f['type'], $color, $f['diameter'] . 'mm']);
+
+            // Update original filament definition
+            if (!isset($f['id'])) {
+                $f = ['id' => Uuid::uuid4()] + $f;
+                file_put_contents($filamentDefinition, json_encode($f, JSON_PRETTY_PRINT));
+            }
+
+            // Set defaults
+            $f['filament_name'] = $filamentName;
+            $f['bridge_fan_speed'] = $this->bridge_fan_speeds[$f['type']];
+            $f['disable_fan_first_layers'] = $this->disable_fan_first_layers[$f['type']];
+            $f['extrusion_multiplier'] = 1;
+            $f['cooling'] = $f['type'] === 'ABS' ? 0 : 1;
+            $f['fan_always_on'] = $f['type'] === 'ABS' ? 0 : 1;
+            $f['k_value'] = ($f['type'] === 'PET') ? 45: 30;
+            $f['filament_notes'] = \sprintf('Calibrated settings for %s.\\n\\n', $filamentName);
+            $f['filament_colour'] = $color;
+            $f['min_fan_speed'] = $this->min_fan_speed[$f['type']];
+            $f['max_fan_speed'] = $this->max_fan_speed[$f['type']];
+            $f['min_print_speed'] = ($f['type'] === 'ABS') ? 5 : 15;
+            $f['fan_below_layer_time'] = $this->fan_below_layer_time[$f['type']];
+            $f['filament_max_volumetric_speed'] = $this->filament_max_volumetric_speed[$f['type']];
+            $f['inherits'] = $this->inherits[$f['type']];
+
+            if (!isset($f['first_layer_bed_temperature'])) {
+                $f['first_layer_bed_temperature'] = 60;
+            }
+
+            if (!isset($f['first_layer_temperature'])) {
+                $f['first_layer_temperature'] = 205;
+            }
+
+            if (!isset($f['next_layer_bed_temperature'])) {
+                $f['next_layer_bed_temperature'] = $f['first_layer_bed_temperature'];
+            }
+
+            if (!isset($f['next_layer_temperature'])) {
+                $f['next_layer_temperature'] = $f['first_layer_temperature'];
+            }
+
+            // Store Cura Material Settings
+            $cura_material_settings[(string) $f['id']] = ['spool_weight' => $f['weight'], 'spool_cost' => $f['price']];
+
+            $this->info($filamentName . ' (' . \basename($filamentDefinition) . ')');
+
+            # Retrieve Linear Advance Calibration data
+            if (isset($f['k_value_calibrations'])) {
+                $kValueCalibration = $f['k_value_calibrations'][0];
+                $kvalue = $kValueCalibration['value'];
+                if (isset($kvalue)) {
+                    $f['k_value'] = $kvalue;
+                    $f['filament_notes'] .= \sprintf('K Value last calibrated on %s\\n', $kValueCalibration['date']);
+                }
+            } else {
+                $this->warn('>> Linear Advance Calibration not performed yet.');
+            }
+
+            # Retrieve Diameter Calibration data
+            if (isset($f['diameter_calibrations'])) {
+                $dm = collect($f['diameter_calibrations']);
+                $avg = $dm->pluck('measurements')->flatten()->average();
+                $margin = $avg - $f['diameter'];
+
+                if (isset($avg)) {
+                    $this->info(\sprintf('Filament Diameter    : %.3fmm [%.3fmm (%.2f%%)]', $avg, $margin, ($margin/$f['diameter'] * 100)));
+                    $f['diameter'] = \sprintf('%.3f', $avg);
+                    $f['filament_notes'] .= \sprintf('Filament diameter last calibrated on %s\\n', $dm->pluck('date')->max());
+                }
+            } else {
+                $this->warn('>> Filament Diameter Calibration not performed yet.');
+            }
+
+            # Retrieve Extrusion Multiplier Calibration data
+            if (isset($f['extrusion_calibrations'])) {
+                $em = collect($f['extrusion_calibrations']);
+
+                $newMultiplier = $em->map(function ($item) {
+                    return ($item['extrusion_width'] / collect($item['measurements'])->average()) * $item['multiplier'];
+                })->average();
+
+                $avg_extrusion_width = $em->pluck('extrusion_width')->flatten()->average();
+
+                $average = $em->pluck('measurements')->flatten()->average();
+                $margin = $average - $avg_extrusion_width;
+
+                if (isset($newMultiplier)) {
+                    $this->info(\sprintf('Extrusion Width      : %.3fmm [%.3fmm (%.2f%%)]', $average, $margin, ($margin/$avg_extrusion_width * 100)));
+                    $this->info(\sprintf('Extrusion Multiplier : %.3f', $newMultiplier));
+
+                    $f['extrusion_multiplier'] = \sprintf('%.3f', $newMultiplier);
+                    $f['filament_notes'] .= \sprintf('Extrusion Multiplier last calibrated on %s\\n', $em->pluck('date')->max());
+                }
+            } else {
+                $this->warn('>> Extrusion Multiplier Calibration not performed yet.');
+            }
+
+            // Export profiles for each supported slicer
+            foreach ($this->slicers as $slicer_id => $slicer_name) {
+                $outputDirectory = storage_path('app/' . self::PROFILES_DIR . DIRECTORY_SEPARATOR . $slicer_id);
+                $profileFilename = $filamentName . '.ini';
+
+                if ('cura' === $slicer_id) {
+                    $profileFilename = \str_replace(' ', '_', $filamentName) . '.xml.fdm_material';
+
+                    // Output Cura Material Settings (replace this in your cura.cfg file)
+                    \file_put_contents($outputDirectory.DIRECTORY_SEPARATOR.'cura_material_settings.txt', 'material_settings = '.\json_encode($cura_material_settings));
+                }
+
+                // Create slicer profile directory if not exists
+                if (!\file_exists($outputDirectory)) {
+                    \mkdir($outputDirectory);
+                }
+
+                // Save output
+                \file_put_contents(
+                    $outputDirectory . DIRECTORY_SEPARATOR . $profileFilename,
+                    $this->plates->render($slicer_id, [
+                        'generated_on' => (new \DateTime())->format(\DATE_ISO8601),
+                        'profile' => $f
+                    ])
+                );
+            }
+            $this->line('');
+        }
+
+        $this->info('Profiles successfully generated for '. \join(', ', $this->slicers));
+    }
+
+    /**
+     * Get the name of a colour based on its hexadecimal code
+     *
+     * @param string $color the colour hexadecimal code
+     * @return string the name of the colour
+     */
+    protected function color2Name(string $color): string
+    {
+        // Strip any preceding hash character
+        $needle = '#';
+        if (\strpos($color, $needle) >= 0) {
+            $color = \substr($color, \strlen($needle));
+        }
+
+        $response = \file_get_contents('http://www.thecolorapi.com/id?hex=' . $color);
+        $color_api = \json_decode($response, true);
+
+        if (isset($color_api['name']['value'])) {
+            return $color_api['name']['value'];
+        }
+
+        // Return the original code if no name can be retrieved
+        return $color;
+    }
+}
