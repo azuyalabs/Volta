d2d6ba58c08e875d2c93a5078ec31492278d27ca
Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>
2019-05-16 21:25:24 +0900

    Correction of user's home view.
    Signed-off-by: Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,7 +34,7 @@ class UserController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function home()
     {
         return view('home');
     }
