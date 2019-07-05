1ff2051a40dc2cd7cfb33f0d90af7574e4a42fba
Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>
2019-07-05 21:45:59 +0900

    Added main Volta class file.
    Signed-off-by: Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>

---
+++ app/Volta.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of the Volta Project.
+ *
+ * Copyright (c) 2018 - 2019. AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <me@sachatelgenhof.com>
+ */
+
+namespace App;
+
+/**
+ * Class Volta
+ *
+ * @package App
+ */
+final class Volta
+{
+    /**
+     * Volta's version number
+     */
+    const VERSION = '0.1.0-dev';
+}
