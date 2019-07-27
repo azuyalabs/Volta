6d765a292f1a29b75b12607f6c4d4620a6c920ce
Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>
2019-07-27 16:26:32 +0900

    Corrected the redirect page after registration, reset password, verification and authenticated redirects. (Fixes issue #12).
    Signed-off-by: Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -35,7 +35,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
