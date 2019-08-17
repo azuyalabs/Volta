1c53358aa1b9924ccabc7b5bedaaf2849c43ca01
Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>
2019-08-17 12:50:27 +0900

    Added default (admin) user.
    Signed-off-by: Sacha Telgenhof Oude Koehorst <me@sachatelgenhof.com>

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,7 +10,11 @@
  * @author Sacha Telgenhof <me@sachatelgenhof.com>
  */
 
+use App\User;
+use App\UserProfile;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 /**
  * Class for seeding the database
@@ -29,6 +33,8 @@ class DatabaseSeeder extends Seeder
             ProductsTableSeeder::class,
         ]);
 
+        $this->createDefaultUser();
+
         // Seed fake data for development
         if (app()->isLocal()) {
             $this->call([
@@ -37,5 +43,35 @@ class DatabaseSeeder extends Seeder
                 FilamentSpoolsTableSeeder::class,
             ]);
         }
+    }
+
+    private function createDefaultUser() : void
+    {
+        $admin = User::create([
+            'name'           => 'Alessandro Volta',
+            'email'          => 'alessandro@volta.prof',
+            'password'       => bcrypt('volta'),
+            'remember_token' => Str::random(10),
+            'api_token'      => Str::random(32),
+        ]);
+        
+        $preferences['dashboard']['clock']['type']                = 'analog';
+        $preferences['dashboard']['weather']['system_of_measure'] = 'metric';
+
+        $profile = UserProfile::create([
+            'currency'    => 'USD',
+            'language'    => 'en-US',
+            'country'     => 'IT',
+            'city'        => 'Como',
+            'preferences' => json_encode($preferences),
+        ]);
+
+        $admin->profile()->save($profile);
+
+
+        $adminRoleName = 'admin';
+        $role          = Role::create(['name' => $adminRoleName]);
+        
+        $admin->assignRole($adminRoleName);
     }
 }
