97f39458cf3e7e552b5fadd79e6ff4f4480e91f2
Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>
2019-08-17 18:28:44 +0900

    Removed welcome modal as it isn't necessary any longer.
    Signed-off-by: Sacha Telgenhof Oude Koehorst <me@sachatelgenhof.com>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,21 +13,6 @@
             </div>
         @endif
 
-        @if (!Auth::user()->last_login)
-            <b-modal id="modal-center" centered ok-only :visible="true">
-                <template slot="modal-header">
-                    <div class="container-fluid text-center">
-                        <img src="{{ url('/images/volta-logo.png') }}" width="205" alt="{{ config('app.name') }}">
-                    </div>
-                </template>
-                <h1 class="display-6 text-center">Hello, {{ Auth::user()->name }}!</h1>
-                <p class="lead">and welcome to {{ config('app.name', 'Laravel') }}! Thank you so much for your help in
-                    testing the first prototype of {{ config('app.name', 'Laravel') }}.</p>
-                <p>With the first step - creating your Volta account - done, please navigate to
-                    the {{ link_to('https://github.com/azuyalabs/Volta/wiki', __('docs.documentation')) }} Wiki to get everything set up.</p>
-            </b-modal>
-        @endif
-
         <div class="row">
             <div class="col-4">
                 <successrate-pie-chart></successrate-pie-chart>
