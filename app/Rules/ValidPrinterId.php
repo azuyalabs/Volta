3761666d93b02db7ec07dea3ac7f4a93fd01da5e
Sacha Telgenhof Oude Koehorst <stelgenhof@gmail.com>
2019-08-20 23:40:46 +0900

    Cleanup. Added conditionals as some native functions can return false in case of an error/failure.
    Signed-off-by: Sacha Telgenhof Oude Koehorst <me@sachatelgenhof.com>

--- app/Rules/ValidPrinterId.php
+++ app/Rules/ValidPrinterId.php
@@ -35,7 +35,6 @@ class ValidPrinterId implements Rule
      * Create a new rule instance.
      *
      * @param string $apiToken the API Token of the user
-     * @return void
      */
     public function __construct(string $apiToken)
     {
@@ -45,23 +44,32 @@ class ValidPrinterId implements Rule
     /**
      * Determine if the validation rule passes.
      *
-     * @param  string $attribute
-     * @param  mixed $value
+     * @param string $attribute
+     * @param mixed $value
      * @return bool
      */
-    public function passes($attribute, $value)
+    public function passes($attribute, $value): bool
     {
         // To decrypt, split the encrypted data from IV - unique separator used was "::"
-        $encodedPrinterId = explode('::', base64_decode(str_replace(['-', '_'], ['+', '/'], $value)));
+        $_dValue = base64_decode(str_replace(['-', '_'], ['+', '/'], $value));
+        if (!$_dValue) {
+            return false;
+        }
+
+        $encodedPrinterId = explode('::', $_dValue);
 
         // Check if the Base64 decoded value is a valid 2 element array
-        if (!is_array($encodedPrinterId) || sizeof($encodedPrinterId) === 1) {
+        if (!is_array($encodedPrinterId) || count($encodedPrinterId) === 1) {
             return false;
         }
 
         // Try to decrypt the encoded printer ID with the given API Token
         // Check if the decoded printer ID matches the string pattern ('<machine>@<host>:<port>')
         $printerIdDecoded = openssl_decrypt($encodedPrinterId[0], 'aes-256-cfb8', $this->apiToken, 1, $encodedPrinterId[1]);
+        if (!$printerIdDecoded) {
+            return false;
+        }
+
         if (preg_match('/^\S+@\S+:\d{1,5}$/', $printerIdDecoded) !== 1) {
             return false;
         }
@@ -76,7 +84,7 @@ class ValidPrinterId implements Rule
      *
      * @return string
      */
-    public function message()
+    public function message(): string
     {
         return 'The printer :attribute is not valid. It must be a properly formatted and encoded string.';
     }
