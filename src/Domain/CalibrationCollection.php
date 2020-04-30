cd831442785c99bb5b9a0a7493002f0e39f47a46
Sacha Telgenhof <me@sachatelgenhof.com>
2020-04-30 22:20:41 +0900

    Created Calibration Collection. Missing files.

---
+++ src/Domain/CalibrationCollection.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Volta\Domain;
+
+use Volta\Domain\Exception\NoCalibrationsException;
+
+class CalibrationCollection
+{
+    private array $calibrations = [];
+
+    public function add(Calibration $calibration): void
+    {
+        $this->calibrations[$calibration->getName()->getValue()][] = $calibration;
+    }
+
+    public function getCalibrations(?string $name = null): array
+    {
+        return is_null($name) ? $this->calibrations: $this->calibrations[$name] ;
+    }
+
+    public function getAverage(string $name):  float
+    {
+        if (!isset($this->calibrations[$name])) {
+            throw new NoCalibrationsException();
+        }
+
+        $result = 0;
+        $c      = $this->calibrations[$name];
+
+        if (0 < count($c)) {
+            $sum  = array_reduce($c, static function ($carry, $item) {
+                $count  = (is_array($carry)) ? $carry[0] : 0;
+                $values = (is_array($carry)) ? $carry[1] : 0;
+
+                return [$count + count($item->getMeasurements()), $values + array_sum($item->getMeasurements())];
+            });
+
+            $result = $sum[1] / $sum[0];
+        }
+
+        return $result;
+    }
+
+    public function getMaximum(string $name): float
+    {
+        if (!isset($this->calibrations[$name])) {
+            throw new NoCalibrationsException();
+        }
+
+        return array_reduce($this->calibrations[$name], static function ($carry, $item) {
+            $m = max($item->getMeasurements());
+
+            return $carry > $m ? $carry : $m;
+        });
+    }
+}
---
+++ src/Domain/Exception/NoCalibrationsException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Volta\Domain\Exception;
+
+class NoCalibrationsException extends \UnexpectedValueException implements Exception
+{
+    public function __construct(
+        string $message = 'there are no calibrations with this name',
+        int $code = 0,
+        \Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
+}
